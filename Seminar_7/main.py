033a2eb928e4c4cc8db492d08ca899c65d6a9b6a
Bronislav <CJLaB4er@gmail.com>
2023-03-19 19:43:25 +0300

    реализован метод поиска по тексту

--- Seminar_7/main.py
+++ Seminar_7/main.py
@@ -4,7 +4,7 @@ while True:
     print(f'Выберите одно из возможных действий программы:\n'
           f'1 - добавить данные в конец файла\n'
           f'2 - вывести все данные из файла\n'
-          # f'3 - поиск данных в файле по строкам\n'
+          f'3 - поиск данных в файле по строкам\n'
           # f'4 - удаление строки\n'
           # f'5 - редактирование строки\n'
           f'5 - выход из программы\n')
@@ -17,8 +17,8 @@ while True:
             with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
                 data = file.readlines()
                 methods.print_data(data)
-        # case 3:
-        #     methods.search_line_in_data()
+        case 3:
+            methods.print_data(methods.search_in_file())
         case 4: # Здесь будет ссылка на метод удаления строк
             pass
         case 5: # Здесь будет ссылка на метод редактировния строки
--- Seminar_7/methods.py
+++ Seminar_7/methods.py
@@ -8,3 +8,14 @@ def print_data(data: list) -> None:
     for line in data:
         print(f'>> {line}', end='')
     print('\n')
+
+def search_in_file() -> list:
+    search = input('Введите информацию для поиска: ')
+    result = list()
+    with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            if search in line:
+                result.append(line)
+        if len(result) < 1:
+            result.append('Поиск не дал результата, попробуйте изменить параметры поиска.')
+        return result
