9801ae9b0a70b7a4d5f3f3cf6dcc6d67f952a455
Bronislav <CJLaB4er@gmail.com>
2023-03-19 19:26:19 +0300

    реализован метод вывода всей информации справочника

--- Seminar_7/main.py
+++ Seminar_7/main.py
@@ -3,17 +3,20 @@ import methods
 while True:
     print(f'Выберите одно из возможных действий программы:\n'
           f'1 - добавить данные в конец файла\n'
-          # f'2 - вывести все данные из файла\n'
+          f'2 - вывести все данные из файла\n'
           # f'3 - поиск данных в файле по строкам\n'
           # f'4 - удаление строки\n'
           # f'5 - редактирование строки\n'
           f'5 - выход из программы\n')
     choice = int(input('Введите номер действия: '))
+    print()
     match choice:
         case 1:
             methods.write_data()
         case 2: # Здесь будет ссылка на метод вывода всего справочника
-            pass
+            with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
+                data = file.readlines()
+                methods.print_data(data)
         # case 3:
         #     methods.search_line_in_data()
         case 4: # Здесь будет ссылка на метод удаления строк
--- Seminar_7/methods.py
+++ Seminar_7/methods.py
@@ -3,3 +3,10 @@ def write_data() -> None:
     with open('telephone_directory.txt', 'a', encoding='utf-8') as file:
         file.write(f'{data} \n')
     print('\nДанные успешно добавлены в справочник\n')
+
+def print_data(data: list) -> None:
+    index = 1
+    for line in data:
+        print(f'{index}. {line}', end='')
+        index += 1
+    print('\n')
