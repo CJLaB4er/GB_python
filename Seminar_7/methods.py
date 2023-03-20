38e7f6d7e608f06e3cfab955c89a31600a4b8044
Bronislav <CJLaB4er@gmail.com>
2023-03-20 22:37:08 +0300

    Исправлен метод поиска - в отобранных строках отсекается символ переноса Исправлены опечатки в интерфейсе

--- Seminar_7/methods.py
+++ Seminar_7/methods.py
@@ -1,21 +1,31 @@
-def write_data(data:list) -> None:
+# Ф-я принимает список значений, и добавляет посточно принятые значения в файл.
+def write_data(data: list) -> None:
     with open('telephone_directory.txt', 'a', encoding='utf-8') as file:
         for line in data:
-            file.write(f'{line} \n')
+            file.write(f'{line}\n')
     print('\nДанные успешно добавлены в справочник\n')
 
+
+# Ф-я принимает список значений и выводит его в консоль в отредактированном виде
 def print_data(data: list) -> None:
     for line in data:
         print(f'>> {line}', end='')
     print('\n')
 
+
+# Внутри ф-и вводится параметр по которому происходит поиск внутри справочника, и возвращает список найденых строк.
 def search_in_file() -> list:
     search = input('Введите информацию для поиска: ')
     result = list()
     with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
         for line in file:
-            if search in line:
-                result.append(line)
+            if search.lower() in line.lower():
+                result.append(line[:-1])  # срез отсекает непечатный символ переноса строки
         if len(result) < 1:
             result.append('Поиск не дал результата, попробуйте изменить параметры поиска.')
         return result
+
+
+# def delete_line_in_file() -> None:
+#     data = search_in_file()
+#     write_data(data)
