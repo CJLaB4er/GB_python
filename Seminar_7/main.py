16f066136dc9f78af197d30f49737e5ea0e97ea2
Bronislav <CJLaB4er@gmail.com>
2023-03-20 23:17:19 +0300

    Реализован метод удаления строк В интерфейс добавлен пункт удаления строк

--- Seminar_7/main.py
+++ Seminar_7/main.py
@@ -5,15 +5,15 @@ while True:
           f'1 - добавить данные в конец файла\n'
           f'2 - вывести все данные из файла\n'
           f'3 - поиск данных в файле по строкам\n'
-          # f'4 - удаление строки\n'
+          f'4 - удаление строки\n'
           # f'5 - редактирование строки\n'
           f'6 - выход из программы\n')
     choice = int(input('Введите номер действия: '))
     print()
     match choice:
         case 1:
-            # data = [input('Введите данные для добавления в справочник: ')]
-            data = ['Первая запись', 'Вторая запись', 'Третья запись']
+            data = [input('Введите данные для добавления в справочник: ')]
+            # data = ['Первая запись', 'Вторая запись', 'Третья запись']
             methods.write_data(data)
         case 2: # Здесь будет ссылка на метод вывода всего справочника
             with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
@@ -21,8 +21,8 @@ while True:
                 methods.print_data(data)
         case 3:
             methods.print_data(methods.search_in_file())
-        case 4: # Здесь будет ссылка на метод удаления строк
-            pass
+        case 4:
+            methods.delete_line_in_file()
         case 5: # Здесь будет ссылка на метод редактировния строки
             pass
         case 6:
--- Seminar_7/methods.py
+++ Seminar_7/methods.py
@@ -1,6 +1,6 @@
 # Ф-я принимает список значений, и добавляет посточно принятые значения в файл.
-def write_data(data: list) -> None:
-    with open('telephone_directory.txt', 'a', encoding='utf-8') as file:
+def write_data(data: list, param: str = 'a') -> None:  # по умолчанию параметр 'a', будет вызов с параметром 'w'
+    with open('telephone_directory.txt', param, encoding='utf-8') as file:
         for line in data:
             file.write(f'{line}\n')
     print('\nДанные успешно добавлены в справочник\n')
@@ -10,7 +10,6 @@ def write_data(data: list) -> None:
 def print_data(data: list) -> None:
     for line in data:
         print(f'>> {line}', end='')
-    print('\n')
 
 
 # Внутри ф-и вводится параметр по которому происходит поиск внутри справочника, и возвращает список найденых строк.
@@ -26,6 +25,13 @@ def search_in_file() -> list:
         return result
 
 
-# def delete_line_in_file() -> None:
-#     data = search_in_file()
-#     write_data(data)
+# Ф-я по введенному параметру находит строку(и) и перезаписывает файл без найденных строк
+def delete_line_in_file() -> None:
+    delete_data = search_in_file()
+    data = list()
+    with open('telephone_directory.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            if line[:-1] not in delete_data:
+                data.append(line[:-1])
+    write_data(data, 'w')
+
